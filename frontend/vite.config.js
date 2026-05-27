f6fd76ddd6f616eeacbd659c2465d5df17ec7757
Tushita <tushitas59@gmail.com>
2026-05-27 14:45:19 +0530

    fixed backend target

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://snaplink-backend-6fum.onrender.com',
         changeOrigin: true,
       },
     },
