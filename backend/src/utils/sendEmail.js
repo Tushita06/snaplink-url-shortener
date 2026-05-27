7230595f531a206a2c698ac78969ce31089e3756
Tushita <tushitas59@gmail.com>
2026-05-27 21:42:34 +0530

    optimize email sending

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,16 +1,21 @@
 const nodemailer = require('nodemailer');
 
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+  family: 4,
+});
 
-  const sendEmail = async ({ to, subject, html }) => {
+const sendEmail = async ({ to, subject, html }) => {
   await transporter.sendMail({
     from: `"SnapLink" <${process.env.EMAIL_USER}>`,
     to,
