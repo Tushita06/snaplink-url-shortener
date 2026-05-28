f767631ff7a900329ec20afd3c5672945a338332
Tushita <tushitas59@gmail.com>
2026-05-28 09:03:18 +0530

    fix email timeout

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,23 +1,33 @@
 const nodemailer = require("nodemailer");
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false,
-  },
-});
-
 const sendEmail = async ({ to, subject, html }) => {
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+
+    tls: {
+      rejectUnauthorized: false,
+    },
+
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000,
+  });
+
   await transporter.sendMail({
     from: `"SnapLink" <${process.env.EMAIL_USER}>`,
     to,
     subject,
     html,
   });
+
 };
 
 module.exports = sendEmail;
